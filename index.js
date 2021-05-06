a1b19e13f5620d707533221fcda845475d81ec74
Alejandro <aleguerra05@gmail.com>
2021-05-06 15:19:22 -0400

    init3

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
+const path = require("path");
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/about', (req, res) => res.send('About Page Route'));
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+});
+
+app.get("/user", (req, res) => {
+    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+});
+
+/*app.get('/about', (req, res) => res.send('About Page Route'));
 
 app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
+app.get('/contact', (req, res) => res.send('Contact Page Route'));*/
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
