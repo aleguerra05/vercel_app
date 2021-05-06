07652dae6177c9186b635dcbceff6dba25550ce5
aleguerra <aleguerra05@gmail.com>
2021-05-06 16:29:05 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,71 @@
 const express = require('express');
 const path = require("path");
-const { auth } = require('express-openid-connect');
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+require("dotenv").config();
+const authRouter = require("./auth");
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'https://vercel-app-red.vercel.app',
-  clientID: 'UcP9P0zYUN3Pv44nxHtmVgauwwVGc2HD',
-  issuerBaseURL: 'https://dev-6nk1awmf.us.auth0.com'
-};
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+    {
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+      /**
+       * Access tokens are used to authorize users to an API
+       * (resource server)
+       * accessToken is the token to call the Auth0 API
+       * or a secured third-party API
+       * extraParams.id_token has the JSON Web Token
+       * profile has all the information from the user
+       */
+      return done(null, profile);
+    }
+  );
+
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+  };
+
+
 
 const app = express();
+
+  
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+  }
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+
+  app.use("/", authRouter);
+
 
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
@@ -25,12 +75,13 @@ app.get("/user", (req, res) => {
     res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
 });
 
-app.use(auth(config));
 
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+
+/**
+ * Session Configuration
+ */
+
+
 
 /*app.get('/about', (req, res) => res.send('About Page Route'));
 
@@ -41,3 +92,4 @@ app.get('/contact', (req, res) => res.send('Contact Page Route'));*/
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+
