72f366b9b7fd545a1c74f3625e7c09bcf21e15df
Alejandro <aleguerra05@gmail.com>
2021-05-07 10:05:35 -0400

    authDone

--- index.js
+++ index.js
@@ -1,19 +1,102 @@
 const express = require('express');
 const path = require("path");
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+require("dotenv").config();
+const authRouter = require("./auth");
+
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+    {
+      domain: process.env.AUTH0_DOMAIN,
+      clientID: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+      /**
+       * Access tokens are used to authorize users to an API
+       * (resource server)
+       * accessToken is the token to call the Auth0 API
+       * or a secured third-party API
+       * extraParams.id_token has the JSON Web Token
+       * profile has all the information from the user
+       */
+      return done(null, profile);
+    }
+  );
+
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+  };
+
+
 
 const app = express();
+
+  
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+  }
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(expressSession(session));
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  
+  passport.deserializeUser((user, done) => {
+    done(null, user);
+  });
+
+  // Creating custom middleware with Express
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  next();
+});
+
+const secured = (req, res, next) => {
+  if (req.user) {
+    return next();
+  }
+  req.session.returnTo = req.originalUrl;
+  res.redirect("/login");
+};
+
+  app.use("/", authRouter);
+
+
 app.get("/", (req, res) => {
     res.render("index", { title: "Home" });
 });
 
-app.get("/user", (req, res) => {
-    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
+app.get("/user",secured, (req, res, next) => {
+    const { _raw, _json, ...userProfile } = req.user;
+    res.render("user", { title: "Profile", userProfile: userProfile });
 });
+
+
+
+/**
+ * Session Configuration
+ */
+
+
 
 /*app.get('/about', (req, res) => res.send('About Page Route'));
 
@@ -24,3 +107,4 @@ app.get('/contact', (req, res) => res.send('Contact Page Route'));*/
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+
